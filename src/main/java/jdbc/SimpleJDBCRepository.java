3bdc99a628760ed8c74ae13eefc1dcfbddb96e43
Armen <mirzoyana805@gmail.com>
2024-05-11 19:15:24 +0400

    Second Commit

--- src/main/java/jdbc/SimpleJDBCRepository.java
+++ src/main/java/jdbc/SimpleJDBCRepository.java
@@ -6,7 +6,6 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.sql.DataSource;
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
@@ -36,31 +35,31 @@ public class SimpleJDBCRepository {
 
 
 
-    public Long createUser(User user) {
-        try {
-            connection = dataSource.getConnection();
-            ps = connection.prepareStatement(createUserSQL);
-            ps.setString(1,user.getFirstName());
-            ps.setString(2,user.getLastName());
-            ps.setInt(3,user.getAge());
-            ps.executeUpdate();
-            ResultSet rs = ps.getGeneratedKeys();
-            if (rs.next()) {
-                return rs.getLong(1);
-            }
-            throw new SQLException("Creating user failed, no ID obtained.");
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }finally {
-            try {
-                connection.close();
-                ps.close();
-                st.close();
-            } catch (SQLException e) {
-                throw new RuntimeException(e);
-            }
-        }
-
+    public Long createUser() {
+//        try {
+//            connection = dataSource.getConnection();
+//            ps = connection.prepareStatement(createUserSQL);
+//            ps.setString(1,user.getFirstName());
+//            ps.setString(2,user.getLastName());
+//            ps.setInt(3,user.getAge());
+//            ps.executeUpdate();
+//            ResultSet rs = ps.getGeneratedKeys();
+//            if (rs.next()) {
+//                return rs.getLong(1);
+//            }
+//            throw new SQLException("Creating user failed, no ID obtained.");
+//        } catch (SQLException e) {
+//            throw new RuntimeException(e);
+//        }finally {
+//            try {
+//                connection.close();
+//                ps.close();
+//                st.close();
+//            } catch (SQLException e) {
+//                throw new RuntimeException(e);
+//            }
+//        }
+return null;
     }
 
     public User findUserById(Long userId) {
@@ -121,32 +120,32 @@ public class SimpleJDBCRepository {
         return users;
     }
 
-    public User updateUser(User user) {
-        try (Connection connection = dataSource.getConnection();
-             PreparedStatement ps = connection.prepareStatement(updateUserSQL)) {
-            ps.setString(1, user.getFirstName());
-            ps.setString(2, user.getLastName());
-            ps.setInt(3, user.getAge());
-            ps.setLong(4, user.getId());
-            ps.executeUpdate();
-
-
-            ResultSet rs = ps.executeQuery();
-            if (rs.next()) {
-                long id = rs.getLong("id");
-                String firstName = rs.getString("firstName");
-                String lastName = rs.getString("lastName");
-                int age = rs.getInt("age");
-
-                return new User(id, firstName, lastName, age);
-            }
-
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-
-        return null;
-
+    public User updateUser() {
+//        try (Connection connection = dataSource.getConnection();
+//             PreparedStatement ps = connection.prepareStatement(updateUserSQL)) {
+//            ps.setString(1, user.getFirstName());
+//            ps.setString(2, user.getLastName());
+//            ps.setInt(3, user.getAge());
+//            ps.setLong(4, user.getId());
+//            ps.executeUpdate();
+//
+//
+//            ResultSet rs = ps.executeQuery();
+//            if (rs.next()) {
+//                long id = rs.getLong("id");
+//                String firstName = rs.getString("firstName");
+//                String lastName = rs.getString("lastName");
+//                int age = rs.getInt("age");
+//
+//                return new User(id, firstName, lastName, age);
+//            }
+//
+//        } catch (SQLException e) {
+//            e.printStackTrace();
+//        }
+//
+//        return null;
+        return  null;
     }
 
     private void deleteUser(Long userId) {
